cf6e731979ecc191a4c80c85d42612459e6320b5
fatimja9 <fatimja9@raspberrypi.local>
2024-04-30 15:50:32 +0100

    update

--- beacontest.py
+++ beacontest.py
@@ -1,15 +1,16 @@
 
-import struct
 import time
 import bluetooth._bluetooth as bluez
 from beacontools import BeaconScanner, IBeaconFilter, IBeaconAdvertisement
 
 uuid = "266B319B-5508-4267-A1B7-678B12E2F02A"
-out_of_range_duration = 10 
+out_of_range_duration = 10
 last_seen_rssi = {}
+last_seen_time = 0
 
 def callback(bt_addr, rssi, packet, additional_info):
     if packet.uuid == uuid:
+        global last_seen_time
         last_seen_time = time.time()
         last_seen_rssi[bt_addr] = rssi
         print("<%s, %d> %s %s" % (bt_addr, rssi, packet, additional_info))
@@ -20,14 +21,15 @@ scanner.start()
 
 try:
     while True:
-        current_time = time.time()
-        absence_duration = current_time - last_seen_time[bt_addr]
-        # for loop?
-        if absence_duration > out_of_range_duration :
-            print(f"Device with address {bt_addr} is out of range.")
-            print(f"Device was absent for", absence_duration, "seconds.")
-
         # Sleep for a short duration before checking again
         time.sleep(2)
-finally:       
+        current_time = time.time()
+        absence_duration = current_time - last_seen_time
+        # for loop?
+        if absence_duration > out_of_range_duration :
+            print(f"Device with address {last_seen_rssi} is out of range.")
+            print(f"Device was absent for", absence_duration, "seconds.")
+
+
+finally:
     scanner.stop()
