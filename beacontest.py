a155d2ad494b2bf152c30aea05ba88cc06f26935
fatimja9 <fatimja9@raspberrypi.local>
2024-04-30 16:06:16 +0100

    runs

--- beacontest.py
+++ beacontest.py
@@ -1,22 +1,23 @@
 
 import time
-import bluetooth._bluetooth as bluez
+#import bluetooth._bluetooth as bluez
 from beacontools import BeaconScanner, IBeaconFilter, IBeaconAdvertisement
 
-uuid = "266B319B-5508-4267-A1B7-678B12E2F02A"
+iuuid = "266b319b-5508-4267-a1b7-678b12e2f02a"
+uuid = "e5b9e3a6-27e2-4c36-a257-7698da5fc140"
 out_of_range_duration = 10
 last_seen_rssi = {}
 last_seen_time = 0
 
 def callback(bt_addr, rssi, packet, additional_info):
-    if packet.uuid == uuid:
+   # if packet.uuid == uuid:
         global last_seen_time
         last_seen_time = time.time()
         last_seen_rssi[bt_addr] = rssi
         print("<%s, %d> %s %s" % (bt_addr, rssi, packet, additional_info))
 
 scanner = BeaconScanner(callback, 
-                        device_filter=IBeaconFilter(uuid=uuid))
+                        device_filter=IBeaconFilter(uuid=iuuid))
 scanner.start()
 
 try:
