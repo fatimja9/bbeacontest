e9234abe336c23bff91bfd5076ffc68447a871a0
fatimja9 <168344639+fatimja9@users.noreply.github.com>
2024-05-17 13:06:08 +0200

    Update Power_measure.py

--- Power_measure.py
+++ Power_measure.py
@@ -9,29 +9,36 @@ i2c = busio.I2C(board.SCL, board.SDA)
 ads = ADS.ADS1115(i2c, address=0x48) # default Adresse 48
 ads.gain = 1
 
-# AnalogIn-Objekts für differenzielle Messung
-chan = AnalogIn(ads, ADS.P0, ADS.P1)
+# AnalogIn-Objekts f  r differenzielle Messung
+#chan = AnalogIn(ads, ADS.P0, ADS.P1)
 
 # Spannungsmessung
-voltage_channel = AnalogIn(ads, ADS.P2)
+voltage_channel = AnalogIn(ads, ADS.P1)
 voltage = voltage_channel.voltage
 
-sample_count = 860      # Samples
+sample_count = 100      # Samples
 current_sum = 0
 shunt_resistor = 0.1    # Ohm
 
 while True:
-  print(f"Spannung: {voltage:.4f} V")
-  
-  # Messung des Stroms über die definierte Anzahl von Samples
+  start = time.time()
+  print(f"Spannung: {voltage:.4f} V") 
+  # Messung des Stroms   ber die definierte Anzahl von Samples
   for _ in range(sample_count):
-    voltage = chan.voltage
+    voltage_channel = AnalogIn(ads, ADS.P1)
+    voltage = voltage_channel.voltage
     current = voltage / shunt_resistor   # I = U / R
     # Aufsummierung der Stromwerte
     current_sum += current
-    time.sleep(1e-3) 
+#    time.sleep(1/sample_count)
+  print(f"Zeitdauer fuer {sample_count} messungen: {time.time() - start}")
 
   # Leistung berechnen und Ausgabe P = UI
-  power = voltage * current_sum
+  power = voltage * current_sum/sample_count
   print(f"Leistung: {power:.4f} W")
-  time.sleep(1)  # Adjust as needed
+  #time.sleep(1)  # Adjust as needed
+
+
+
+
+
