09b472e659089c05608b281e41f47411279195e5
fatimja9 <168344639+fatimja9@users.noreply.github.com>
2024-05-17 11:13:01 +0200

    Update Power_measure.py

--- Power_measure.py
+++ Power_measure.py
@@ -7,26 +7,29 @@ from adafruit_ads1x15.analog_in import AnalogIn
 # I2C Bus erzeugen
 i2c = busio.I2C(board.SCL, board.SDA)
 ads = ADS.ADS1115(i2c, address=0x48) # default Adresse 48
+ads.gain = 1
 
-# single-ended input on channel 0
-chan = AnalogIn(ads, ADS.P0)
+# AnalogIn-Objekts für differenzielle Messung
+chan = AnalogIn(ads, ADS.P0, ADS.P1)
 
 # Spannungsmessung
-voltage = chan.voltage
+voltage_channel = AnalogIn(ads, ADS.P2)
+voltage = voltage_channel.voltage
 
-# Anzahl der Samples für die Strommessung
-sample_count = 860
+sample_count = 860  
 current_sum = 0
-
 shunt_resistor_ohms = 0.1 
 
 while True:
   print(f"Spannung: {voltage:.4f} V")
   
   # Messung des Stroms über die definierte Anzahl von Samples
-  # for _ in range(sample_count):
-    # Messung des Stroms und Aufsummierung
-    #  current_sum += current_channel.value ???????
+  for _ in range(sample_count):
+    voltage = chan.voltage
+    current = voltage / shunt_resistor_value    # I = U / R
+    # Aufsummierung der Stromwerte
+    current_sum += current
+    time.sleep(1e-3) 
 
   # Leistung berechnen und Ausgabe P = UI
   power = voltage * current_sum
